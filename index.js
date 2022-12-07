3b84a132f12487fab1d6e287b02b43db2fb71cfa
Rudis <rudisjcg@gmail.com>
2022-12-07 16:58:00 -0400

    search modifications

--- index.js
+++ index.js
@@ -6,11 +6,13 @@
 
 
 const moviesList = document.querySelector(".books")
+let contrastToggle = false;
+const scaleFactor = 1 / 20;
 
 async function main() {
     const movies = await fetch ("https://www.omdbapi.com/?apikey=97f5b4c9&s=spider");
     const moviesData = await movies.json();
-    console.log(moviesData)
+   
     moviesList.innerHTML = moviesData.Search.map((movie) => moviesHTML(movie)).join("");
     
    
@@ -27,16 +29,27 @@ function moviesHTML(movie) {
         <img class="book_img" src=${movie.Poster} alt="">
     </figure>
     <div class="book__title">
-        ${movie.Title}
+        ${movie.Title} - ${movie.Year}
     </div>
     <div class="book__ratings">
-        <i class="fas fa-star"></i>
-        <i class="fas fa-star"></i>
-        <i class="fas fa-star"></i>
-        <i class="fas fa-star"></i>
-        <i class="fas fa-star-half-alt"></i>
+        ${movie.Type}
     </div>
     
 </div>`
 }
+
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+        document.body.classList += " dark-theme"
+
+    } else {
+        document.body.classList.remove("dark-theme")
+    }
+}
+
+
+
+
 
--- search.js
+++ search.js
@@ -1,24 +1,55 @@
 const moviesSearchList = document.querySelector(".latest_books")
-const title = localStorage.getItem("title")
+const searchBar = document.getElementById('searchBar')
 
+
+let moviesSearchData = [];
+
+
+/*searchBar.addEventListener('keyup', (e) => {
+    const searchString = e.target.value
+    console.log(searchString)
+    renderMovies(e)
+}) 
 async function onSearchChange(event) {
     
-    let name = event.target.title;
+    let name = event.target.value;
     renderMovies(name)
     console.log(renderMovies(name))
 
 }
+const searchTitle = document.getElementsById("nameofM");
+    searchTitle.innerHTML = `${name}`
+*/
 
 
-async function renderMovies(title) {
-    const moviesSearch = await fetch (`https://www.omdbapi.com/?apikey=97f5b4c9&s=${title}`)
-    const moviesSearchData = await moviesSearch.json();
-    console.log(moviesSearch)
-    moviesSearchList.innerHTML = moviesSearchData.Search.map((movieS) => moviesSearchPost(movieS)).join("");
-    
+async function onSearchChange(event) {
+
+    let name = event.target.value;
+
+    renderMovies(name)
+
+
 }
 
-renderMovies(title)
+function searchMovies() {
+}
+
+
+
+
+
+async function renderMovies(name) {
+    const moviesSearch = await fetch(`https://www.omdbapi.com/?apikey=97f5b4c9&s=${name}`)
+    moviesSearchData = await moviesSearch.json();
+    console.log(moviesSearch)
+
+    moviesSearchList.innerHTML = moviesSearchData.Search.map((movieS) => moviesSearchPost(movieS)).join("");
+
+}
+
+
+
+renderMovies("avengers")
 
 
 function moviesSearchPost(movieS) {
@@ -27,15 +58,11 @@ function moviesSearchPost(movieS) {
         <img class="book_img" src=${movieS.Poster} alt="">
     </figure>
     <div class="book__title">
-        ${movieS.Title}
+        ${movieS.Title} - ${movieS.Year}
     </div>
     <div class="book__ratings">
-        <i class="fas fa-star"></i>
-        <i class="fas fa-star"></i>
-        <i class="fas fa-star"></i>
-        <i class="fas fa-star"></i>
-        <i class="fas fa-star-half-alt"></i>
-    </div>
-    
+         ${movieS.Type}</div>
 </div>`
 }
+
+
